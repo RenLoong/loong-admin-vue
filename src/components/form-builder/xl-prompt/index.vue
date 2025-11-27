7b0322a7bbd50b0d3543d2810c8e166ac323ae27
Unknown <unknown.renlong@gmail.com>
2025-11-27 11:48:32 +0800

    prompt custom component

--- src/components/form-builder/xl-prompt/index.vue
+++ src/components/form-builder/xl-prompt/index.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
+import xlCode from '@/components/form-builder/xl-code/index.vue';
+import xlMarkedText from '@/components/form-builder/xl-marked-text/index.vue';
+import xlCopy from '@/components/form-builder/xl-copy/index.vue';
+import xlCountdown from '@/components/form-builder/xl-countdown/index.vue';
+import xlQrcode from '@/components/form-builder/xl-qrcode/index.vue';
 const props = withDefaults(defineProps<{
     prompt: any
 }>(), {
     prompt: []
 });
-
+console.log(props.prompt);
 const getComponentName = (component: string) => {
     const customComponents = ['code','marked-text','copy','countdown','qrcode'];
     if (customComponents.includes(component)) {
-        return 'xl-' + component;
+        switch(component){
+            case 'code':
+                return xlCode;
+            case 'marked-text':
+                return xlMarkedText;
+            case 'copy':
+                return xlCopy;
+            case 'countdown':
+                return xlCountdown;
+            case 'qrcode':
+                return xlQrcode;
+        }
     }
     return 'el-' + component;
 }
@@ -18,7 +34,7 @@ const getComponentName = (component: string) => {
     <div class="flex flex-y-center flex-wrap" v-for="(item, index) in props.prompt" :index="index">
         <component v-for="(c, cindex) in item" :index="cindex" :is="getComponentName(c.component)" v-bind="c.props">
             <template v-for="(slot, _slot) in c.children" v-slot="_slot">
-                <component :is="slot.component" v-bind="slot.props" v-if="typeof slot==='object'"/>
+                <component :is="getComponentName(slot.component)" v-bind="slot.props" v-if="typeof slot==='object'"/>
                 <span v-else>{{slot}}</span>
             </template>
         </component>
